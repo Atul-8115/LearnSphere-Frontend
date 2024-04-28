f15ff843181577c888709a15e7ad1de26a434eaf
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-04-28 17:54:55 +0530

    Add new components and pages for authentication

--- src/components/core/Auth/SignupForm.jsx
+++ src/components/core/Auth/SignupForm.jsx
@@ -1,13 +1,70 @@
 import React, { useState } from 'react'
+import { toast } from 'react-hot-toast'
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+import { setSignupData } from "../../../slices/authSlice"
+import { sendOtp } from "../../../services/operations/authApi"
 import Tab from '../../common/Tab'
 import { ACCOUNT_TYPE } from '../../../utils/constaints'
 
 
 const SignupForm = () => {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
 
+    const [showPassword, setShowPassword] = useState(false)
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false)
+
+    const [formData, setFormData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
     // student or instructor
     const [accountType, setAccountType] = useState(ACCOUNT_TYPE.STUDENT)
 
+    // Handling chnages
+    const handleOnChange = (e) => {
+        setFormData((prevData) => ({
+            ...prevData,
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    // Handle form submission
+    const {firstName, lastName, email, password, confirmPassword} = formData
+    const handleOnSumbit = (e) => {
+        e.preventDefault()
+
+        if(password !== confirmPassword) {
+            toast.error("Password Do Not Match")
+            return
+        }
+        const signupData = {
+            ...formData,
+            accountType,
+        }
+        
+        // Setting signup data to state
+        // To be used after otp verification
+        dispatch(setSignupData(signupData))
+        // Send OTP to user for verification
+        dispatch(sendOtp(formData.email, navigate))
+
+        // Reset
+        setFormData({
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: "",
+            confirmPassword: "", 
+        })
+        setAccountType(ACCOUNT_TYPE.STUDENT)
+    }
     // Data to pass to tab component
     const tabData = [
         {
@@ -17,7 +74,8 @@ const SignupForm = () => {
         },
         {
             id: 2,
-            tabName: ACCOUNT_TYPE.INSTRUCTOR
+            tabName: "Instructor",
+            type: ACCOUNT_TYPE.INSTRUCTOR
         }
     ]
 
@@ -26,32 +84,128 @@ const SignupForm = () => {
       {/* Tab */}
       <Tab tabData={tabData} field={accountType} setField={setAccountType}/>
       {/* Form */}
-      <form>
-         <div>
+      <form 
+         onSubmit={handleOnSumbit}
+         className="flex w-full flex-col gap-y-4">
+         <div className="flex gap-x-4">
             <label>
-                <p>
-                    First Name
+                <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+                    First Name <sup className='text-pink-200'>*</sup>
                 </p>
                 <input
                     required
                     type='text'
                     name="firstName"
-                    // value={firstName}
+                    value={firstName}
+                    onChange={handleOnChange}
                     placeholder='Enter your first name'
+                    style={{
+                       boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                     }}
+                   className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] text-richblack-5"
                 />
             </label>
             <label>
-                <p>
-                    Last Name
+                <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+                    Last Name <sup className="text-pink-200">*</sup>
                 </p>
                 <input
                     required
                     type='text'
                     name='lastName'
+                    value={lastName}
+                    onChange={handleOnChange}
                     placeholder='Enter your last name'
+                    style={{
+                       boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                     }}
+                   className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] text-richblack-5"
                 />
             </label>
          </div>
+         <label className='w-full'>
+            <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+                Email Address <sup className="text-pink-200">*</sup>
+            </p>
+            <input
+                required
+                type='text'
+                name='email'
+                value={email}
+                onChange={handleOnChange}
+                placeholder='Enter your email address'
+                style={{
+                  boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                }}
+                className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] text-richblack-5"
+            />
+         </label>
+         <div className='flex gap-x-4'>
+            <label className='relative'>
+                <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+                   Create Password <sup className="text-pink-200">*</sup>
+                </p>
+                <input
+                    required
+                    type={showPassword ? "text" : "password"}
+                    name='password'
+                    value={password}
+                    onChange={handleOnChange}
+                    placeholder='Enter password'
+                    style={{
+                        boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                    }}
+                   className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] pr-10 text-richblack-5"
+                />
+                <span
+                   onClick={() => setShowPassword((prev) => !prev)}
+                   className='absolute z-[10] right-3 top-[38px] cursor-pointer'
+                >
+                   {
+                      showPassword ? (
+                        <AiOutlineEye fontSize={24} fill="#AFB2BF"/>
+                      ) : (
+                        <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF"/>
+                      )
+                   }
+                </span>
+            </label>
+            <label className='relative'>
+                <p className="mb-1 text-[0.875rem] leading-[1.375rem] text-richblack-5">
+                   Confirm Password <sup className="text-pink-200">*</sup>
+                </p>
+                <input
+                    required
+                    type={showConfirmPassword ? 'text' : 'password'}
+                    name='confirmPassword'
+                    value={confirmPassword}
+                    onChange={handleOnChange}
+                    placeholder='Confirm password'
+                    style={{
+                        boxShadow: "inset 0px -1px 0px rgba(255, 255, 255, 0.18)",
+                    }}
+                   className="w-full rounded-[0.5rem] bg-richblack-800 p-[12px] pr-10 text-richblack-5"
+                />
+                <span
+                   onClick={() => setShowConfirmPassword((prev) => !prev)}
+                   className='absolute z-[10] right-3 top-[38px] cursor-pointer'
+                >
+                    {
+                        showConfirmPassword ? (
+                            <AiOutlineEye  fontSize={24} fill="#AFB2BF"/>
+                        ) : (
+                            <AiOutlineEyeInvisible fontSize={24} fill="#AFB2BF"/>
+                        )
+                    }
+                </span>
+            </label>
+         </div>
+         <button
+               type='submit'
+               className='mt-6 rounded-[8px] bg-yellow-50 py-[8px] px-[12px] font-medium text-richblack-900'
+         >
+                Create Account
+         </button>
       </form>
     </div>
   )
--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,14 @@
-const BASE_URL = 'http://localhost:5173/'
+const BASE_URL = 'http://localhost:8000/api/v1'
 // const BASE_URL = process.env.REACT_APP_BASE_URL
+
+// AUTH ENPOINTS
+export const authEndpoints = {
+    SENDOTP_API: BASE_URL + "/auth/sendotp",
+    LONGIN_API: BASE_URL + "/auth/login",
+    SIGNUP_API: BASE_URL + "/auth/signup",
+    RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
+    RESETPASSWORD_API: BASE_URL + "/auth/reset-password"
+}
 
 export const categories = {
     CATEGORIES_API : BASE_URL + '/course/showAllCategories'
