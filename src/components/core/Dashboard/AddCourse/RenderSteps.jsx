6bcdaaa948c14cf2821d34d63972eb3038f32995
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-16 00:43:24 +0530

    feat: Add PublishCourse component for handling course publishing in AddCourse page

--- src/components/core/Dashboard/AddCourse/RenderSteps.jsx
+++ src/components/core/Dashboard/AddCourse/RenderSteps.jsx
@@ -3,6 +3,7 @@ import { FaCheck } from 'react-icons/fa';
 import { useSelector } from 'react-redux'
 import CourseBuilderForm from './CourseBuilder/CourseBuilderForm'
 import CourseInformationForm from './CourseInformation/CourseInformationForm';
+import PublishCourse from './PublishCourse';
 
 const RenderSteps = () => {
 
@@ -83,7 +84,7 @@ const RenderSteps = () => {
       {/* Render specific component based on current step */}
       {step === 1 && <CourseInformationForm />}
       {step === 2 && <CourseBuilderForm />}
-      {/* {step === 3 && <PublishCourse />} */}
+      {step === 3 && <PublishCourse />}
     </>
   )
 }
