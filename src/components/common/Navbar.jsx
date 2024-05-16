e17ec03687734fd32b655ae4831df63b8556d786
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-16 13:48:16 +0530

    feat: Add swiper package for carousel functionality in the Catalog page

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link, matchPath } from 'react-router-dom'
 import {NavbarLinks} from "../../data/navbar-links"
 import { useLocation } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import {AiOutlineShoppingCart} from "react-icons/ai"
+import {AiOutlineMenu, AiOutlineShoppingCart} from "react-icons/ai"
 import ProfileDropDown from '../core/Auth/ProfileDropDown'
 import { apiConnector } from '../../services/apiconnector'
 import { categories } from '../../services/apis'
 import { useState } from 'react'
 import {IoIosArrowDropdownCircle} from "react-icons/io"
-
+import { BsChevronDown } from "react-icons/bs"
 
 
 const Navbar = () => {
@@ -54,43 +54,52 @@ const Navbar = () => {
       </Link>
 
       {/* Nav Links */}
-      <nav>
+      <nav className="hidden md:block">
         <ul className='flex gap-x-6 text-richblack-25'>
         {
             NavbarLinks.map( (link, index) => (
                  <li key={index}>
                     {
                         link.title === "Catalog" ? (
-                            <div className='relative flex items-center gap-2 group'>
-                                <p>{link.title}</p>
-                                <IoIosArrowDropdownCircle/>
-
-                                <div className='invisible absolute left-[50%]
-                                    translate-x-[-50%] translate-y-[80%]
-                                 top-[50%]
-                                flex flex-col rounded-md bg-richblack-5 p-4 text-richblack-900
-                                opacity-0 transition-all duration-200 group-hover:visible
-                                group-hover:opacity-100 lg:w-[300px] z-20'>
-
-                                <div className='absolute left-[50%] top-0
-                                translate-x-[80%]
-                                translate-y-[-45%] h-6 w-6 rotate-45 rounded bg-richblack-5 z-50'>
-                                </div>
-
-                                {
-                                    subLinks.length ? (
-                                        subLinks.map( (subLink, index) => (
-                                                <Link to='/' key={index}>
-                                                    <p>{subLink.name}</p>
-                                                </Link>
-                                            ) )
-                                    ) : (<div></div>)
-                                }
-
-                                </div>
-
-
-                            </div>
+                            <>
+                    <div
+                      className={`group relative flex cursor-pointer items-center gap-1 ${
+                        matchRoute("/catalog/:catalogName")
+                          ? "text-yellow-25"
+                          : "text-richblack-25"
+                      }`}
+                    >
+                      <p>{link.title}</p>
+                      <BsChevronDown />
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                        {loading ? (
+                          <p className="text-center">Loading...</p>
+                        ) : subLinks.length ? (
+                          <>
+                            {subLinks
+                              ?.filter(
+                                (subLink) => subLink?.courses?.length > 0
+                              )
+                              ?.map((subLink, i) => (
+                                <Link
+                                  to={`/catalog/${subLink.name
+                                    .split(" ")
+                                    .join("-")
+                                    .toLowerCase()}`}
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  key={i}
+                                >
+                                  <p>{subLink.name}</p>
+                                </Link>
+                              ))}
+                          </>
+                        ) : (
+                          <p className="text-center">No Courses Found</p>
+                        )}
+                      </div>
+                    </div>
+                  </>
 
                         ) : (
                             <Link to={link?.path}>
@@ -149,8 +158,9 @@ const Navbar = () => {
             }
             
         </div>
-
-
+        <button className='mr-4 md:hidden'>
+            <AiOutlineMenu fontSize={24} fill='#AFB2BF'/>
+        </button>
       </div>
     </div>
   )
