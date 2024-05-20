54b61377f7581e3d8b332e543d134638f041e71c
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-21 02:36:01 +0530

    feat: Add react-markdown package for rendering markdown content
    This commit adds the react-markdown package to the project's dependencies in the package.json file. It also includes the necessary import statement in the VideoDetails.jsx, CourseReviewModal.jsx, and VideoDetailsSidebar.jsx components. This package will enable the rendering of markdown content in these components, enhancing the user experience and allowing for more dynamic content display.

---
+++ src/pages/ViewCourse.jsx
@@ -0,0 +1,44 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet, useParams } from 'react-router'
+import { getFullDetailsOfCourse } from '../services/operations/courseDetailsApi';
+import { setCompletedLectures, setCourseSectionData, setEntireCourseData, setTotalNoOfLectures } from '../slices/viewCourseSlice';
+import VideoDetailsSidebar from '../components/core/ViewCourse/VideoDetailsSidebar';
+import CourseReviewModal from '../components/core/ViewCourse/CourseReviewModal';
+
+const ViewCourse = () => {
+
+    const [reviewModal, setReviewModal] = useState(false)
+    const {courseId} = useParams();
+    const {refreshToken} = useSelector((state) => state.auth)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const setCourseSpecificDetails = async () => {
+            const courseData = await getFullDetailsOfCourse(courseId, refreshToken)
+            dispatch(setCourseSectionData(courseData.courseDetails.courseContent));
+              dispatch(setEntireCourseData(courseData.courseDetails));
+              dispatch(setCompletedLectures(courseData.completedVideos));
+              let lectures = 0;
+              courseData?.courseDetails?.courseContent?.forEach((sec) => {
+                lectures += sec.subSection.length
+              })
+              dispatch(setTotalNoOfLectures(lectures))
+        }
+        setCourseSpecificDetails();
+    },[])
+  return (
+    <>
+     <div>
+        <VideoDetailsSidebar setReviewModal={setReviewModal}/>
+        <div>
+            <Outlet/>
+        </div>
+     </div> 
+     {reviewModal && <CourseReviewModal setReviewModal={setReviewModal}/>}
+    </>
+  )
+}
+
+export default ViewCourse
